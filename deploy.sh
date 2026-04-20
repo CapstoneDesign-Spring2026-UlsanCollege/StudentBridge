2acb2f23f7dca3ccbdbafa9adb77dfc8501ab806
Mezbah Uddin <163080472+Mezbah143@users.noreply.github.com>
2026-04-20 19:23:05 +0900

    clean structure

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ rsync -av --exclude ".git" --exclude ".vscode" "$PROJECT/" "$TOMCAT/"
 javac -cp "$PROJECT/docs/mysql-connector-j-9.3.0.jar:$PROJECT/docs/servlet-api.jar" -d "$PROJECT/bin" "$PROJECT"/Backend/*.java
 
 cp "$PROJECT"/bin/*.class "$TOMCAT/WEB-INF/classes/"
-cp "$PROJECT/docs/mysql-connector-j-9.3.0.jar" "$TOMCAT/WEB-INF/lib/"
+cp "$PROJECT/server/mysql-connector-j-9.3.0.jar" "$TOMCAT/WEB-INF/lib/"
 
 echo "Deployment Complete!"
